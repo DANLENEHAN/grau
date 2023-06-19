e0fc784c5c2b14f81902de49e9535ba25f5897da
Daniel Maguire <dmaguire1996@gmail.com>
2023-06-19 11:20:28 +0100

    Added another test

--- tests/grau/test_utils.py
+++ tests/grau/test_utils.py
@@ -17,7 +17,8 @@ class TestUtils:
 
     def test_get_secret_key(self):
         """
-        Test that the function returns the APP_SECRET environment variable.
+        Test that the function returns the APP_SECRET
+        environment variable.
         """
         # given
         expected = self.app_secret
@@ -27,9 +28,11 @@ class TestUtils:
         # then
         assert actual == expected
 
-    def test_get_secret_key_none(self):
+    def test_get_secret_key_none(self):  # noqa pylint: disable=R0201
         """
-        Test that the function returns None if the APP_SECRET environment variable is not set.
+        Test that the function returns None if
+        the APP_SECRET
+        environment variable is not set.
         """
         # given
         del os.environ["APP_SECRET"]
@@ -41,7 +44,8 @@ class TestUtils:
     @patch("grau.utils.Fernet")
     def test_encrypt_str(self, mock_fernet: MagicMock):
         """
-        Test that the function encrypts a string using a secret key.
+        Test that the function encrypts a string using a
+        secret key.
         """
         # given
         secret_str = self.test_password
@@ -49,8 +53,9 @@ class TestUtils:
         encrypt_str(secret_str, self.app_secret)
         # then
         mock_fernet.assert_called_once_with(self.app_secret)
-        mock_fernet.return_value.encrypt.assert_called_once_with(secret_str.encode())
-        mock_fernet.return_value.encrypt.return_value.decode.assert_called_once()
+        encrypt = mock_fernet.return_value.encrypt
+        encrypt.assert_called_once_with(secret_str.encode())
+        encrypt.return_value.decode.assert_called_once()
 
     @patch("grau.utils.Fernet")
     def test_decrypt_str(self, mock_fernet: MagicMock):
@@ -63,5 +68,20 @@ class TestUtils:
         decrypt_str(secret_str, self.app_secret)
         # then
         mock_fernet.assert_called_once_with(self.app_secret)
-        mock_fernet.return_value.decrypt.assert_called_once_with(secret_str.encode())
-        mock_fernet.return_value.decrypt.return_value.decode.assert_called_once()
+
+        decrypt = mock_fernet.return_value.decrypt
+        decrypt.assert_called_once_with(secret_str.encode())
+        decrypt.return_value.decode.assert_called_once()
+
+    def test_decrypt_after_encrypt(self):
+        """
+        Test that the function decrypts a string
+        after it was encrypted.
+        """
+        # given
+        secret_str = self.test_password
+        # when
+        encrypted_str = encrypt_str(secret_str, self.app_secret)
+        actual = decrypt_str(encrypted_str, self.app_secret)
+        # then
+        assert actual == secret_str
