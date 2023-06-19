eb21997649c9f403b9e154d826d6a26fe73eedef
Dan Lenehan <daniel.lenehan.work@gmail.com>
2023-06-19 19:08:30 +0100

    GRAU-5 & GRAU-13 (#4)

---
+++ grau/db/enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class HeightUnits(Enum):
+    LB = "lbs"
+    KG = "kg"
+    STONE = "stone"
+
+
+class WeightUnits(Enum):
+    CM = "cm"
+    INCH = "inch"
+    FEET = "feet"
--- grau/db/functions.py
+++ grau/db/functions.py
@@ -5,10 +5,12 @@ from sqlalchemy.orm import sessionmaker
 
 
 def get_db_engine() -> Engine:
+    """Get the database engine."""
     db_url = os.environ.get("DATABASE_URL")
     db_url = db_url or "postgresql+psycopg2://dan:testing123@localhost/train"
     return create_engine(db_url)
 
 
 def get_session_maker() -> sessionmaker:
+    """Get the session maker."""
     return sessionmaker(bind=get_db_engine())
